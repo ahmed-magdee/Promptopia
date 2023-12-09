a6a1121121bfd08cb5562bf7f3e181ec5c78505e
unknown <am247249@gmail.com>
2023-12-09 20:39:43 +0200

    fix type

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -51,7 +51,7 @@ const Nav = () => {
             </button>
             <Link href={"/profile"}>
               <Image
-                src={session?.user?.image}
+                src={session?.user.image}
                 width={37}
                 height={37}
                 className="rounded-full"
